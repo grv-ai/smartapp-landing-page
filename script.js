ce7a850ef7c2712e96160c34635e312cf7c810d1
Gaurav Joshi <grv55980@gmail.com>
2026-01-15 17:45:19 +0530

    Initial commit - SmartApp landing page

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+// Lazy load images
+document.querySelectorAll("img").forEach(img => {
+  img.loading = "lazy";
+});
