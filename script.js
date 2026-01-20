2c4f88426ef17543bd6f60809da366c499d70056
Gaurav Joshi <grv55980@gmail.com>
2026-01-20 08:34:27 +0530

    change in full code

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// Smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener("click", e => {
-    e.preventDefault();
-    document.querySelector(link.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
-  });
-});
-
-// Lazy load images
-document.querySelectorAll("img").forEach(img => {
-  img.loading = "lazy";
-});
+alert("Welcome to SmartApp!");
